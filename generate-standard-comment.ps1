7e8199596edcf604fd2fba60931f2a6a06efc3a4
Daniel Tran <daniel.tran11@gmail.com>
2022-01-15 12:30:34 +1100

    Parameterise mentions and hashtags in comment generating script

--- generate-standard-comment.ps1
+++ generate-standard-comment.ps1
@@ -10,19 +10,32 @@
     social media platforms such as LinkedIn.
 .PARAMETER commentsFile
     The file of preset comments to extract a sample from.
+    Defaults to a pre-existing file.
+.PARAMETER mentions
+    The mentions used to refer to specific entities like people or companies.
+    Defaults to the empty string.
+.PARAMETER hashtags
+    The hashtags used to tag the post with categories. Defaults to the empty string.
 .EXAMPLE
     .\generate-standard-comment.ps1
 .EXAMPLE
     .\generate-standard-comment.ps1 -commentsFile .\COMMENTS.txt
+.EXAMPLE
+    .\generate-standard-comment.ps1 -mentions "@Macrohard @Snuckeys"
+.EXAMPLE
+    .\generate-standard-comment.ps1 -hashtags "#technology"
 .NOTES
     Currently only tested on LinkedIn.
 #>
 
 param(
-   [string]$commentsFile = ".\COMMENTS.txt"
+   [string]$commentsFile = ".\COMMENTS.txt",
+   [string]$mentions = "",
+   [string]$hashtags = ""
 )
 
 # Random line selecting logic is based on this Microsoft post:
 # https://devblogs.microsoft.com/scripting/use-powershell-to-pick-random-winning-users-from-text/
 $comment = Get-Content $commentsFile | sort{Get-Random} | select -First 1
-"${comment} @Macrohard #technology"
+# Double space replacement is mainly to handle the case when $mentions is empty
+"${comment} ${mentions} ${hashtags}" -replace "  ", " "
